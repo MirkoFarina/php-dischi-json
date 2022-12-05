31a24dbfbce35970d700891d867b764554ef7966
MirkoFarina <MirkoFarina@outlook.com>
2022-12-05 14:42:59 +0100

    [add] html

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <!-- Font awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- CSS -->
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
     <!-- Axios -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- VueJs -->
@@ -18,7 +18,41 @@
 </head>
 <body>
     <div id="app">
+        <header>
+            <div class="container">
+                <div class="logo">
+                    <img src="./assets/img/logo-small.svg" alt="Logo Spotify">
+                </div>
+            </div>
+        </header>
 
+        <main>
+            <div class="container">
+                <div class="row row-cols-3 my-4">
+                    <div class="col my-5">
+                        <div class="mf-card w-100">
+                            <div class="top w-100">
+                                <div class="img w-100"></div>
+                            </div>
+                            <div class="bottom text-center">
+                                <div class="content-bottom">
+                                    <h2>
+                                        title
+                                    </h2>
+                                    <h5>
+                                        artist
+                                    </h5>
+                                    <h6>
+                                        data
+                                    </h6>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     </div>
+    <script src="./assets/js/main.js"></script>
 </body>
 </html>
